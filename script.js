a8d04e67fc8887d27118a961d7e0f551ecff7436
Kaylee Ortega <>
2023-03-31 14:39:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let yesButton= document.querySelector(".option-one")
+let noButton;
+
+
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+*/
+
